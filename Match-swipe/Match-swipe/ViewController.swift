3623f3b337bc9c7af0e39bef7c8dfc197f05a17a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2018-12-24 08:16:27 -0500

    Added vertical stackView

--- Match-swipe/Match-swipe/ViewController.swift
+++ Match-swipe/Match-swipe/ViewController.swift
@@ -14,67 +14,42 @@ class ViewController: UIViewController {
     
     let redView : UIView = {
        let view = UIView()
-       view.backgroundColor = .red
+       view.backgroundColor = .blue
        view.translatesAutoresizingMaskIntoConstraints = false
        return view
     }()
     
     let yellowView : UIView = {
         let view = UIView()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .orange
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let blueView : UIView = {
         let view = UIView()
-        view.backgroundColor = .blue
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let orangeView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .orange
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let purpleView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .purple
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let greenView : UIView = {
-        let view = UIView()
         view.backgroundColor = .green
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
-    let blackView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .black
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
     
-    let whiteView : UIView = {
-        let view = UIView()
-        view.backgroundColor = .purple
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        [redView , yellowView , blueView, orangeView, purpleView, greenView, blackView, whiteView].forEach {
-            view.addSubview($0)
-        }
         setupConstraints()
+        stackViewsVertical()
+    }
+    
+    func stackViewsVertical(){
+        let stackView = UIStackView(arrangedSubviews: [redView, yellowView, blueView])
+        view.addSubview(stackView)
+        
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: 0))
+        
     }
     
     
@@ -92,27 +67,12 @@ class ViewController: UIViewController {
     
     func setupConstraints(){
         
-        redView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 100))
+//        redView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 100))
+//
+//        yellowView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 100))
+//
+//        blueView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.leadingAnchor, bottom: yellowView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 30, bottom: 0, right: -30))
         
-        yellowView.anchor(top: redView.bottomAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 100))
-        
-        blueView.anchor(top: self.view.safeAreaLayoutGuide.topAnchor, leading: self.view.leadingAnchor, bottom: yellowView.bottomAnchor, trailing: redView.leadingAnchor, padding: .init(top: 0, left: 30, bottom: 0, right: -30))
-        
-        orangeView.anchor(top: yellowView.bottomAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 50, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 0))
-        
-        orangeView.anchorSize(to: yellowView)
-        
-        purpleView.anchor(top: orangeView.bottomAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: -10), size: .init(width: 100, height: 0))
-        
-        purpleView.anchorSize(to: orangeView)
-        
-        greenView.anchor(top: self.blueView.bottomAnchor, leading: self.view.leadingAnchor, bottom: purpleView.bottomAnchor, trailing: orangeView.leadingAnchor, padding: .init(top: 50, left: 30, bottom: 0, right: -30))
-        
-        blackView.anchor(top: purpleView.bottomAnchor, leading: nil, bottom: nil, trailing: self.view.trailingAnchor, padding: .init(top: 10, left: 0, bottom: 0, right: -10))
-        
-        blackView.anchorSize(to: purpleView)
-        
-        whiteView.fillSuperView()
     }
 }
 
